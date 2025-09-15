4a3d92f944f2602f951d3e042d168635273ea2f6
Minh Trung <trungho.234416@gmail.com>
2025-09-16 00:54:06 +0700

    fix

--- src/main/java/posts_management/backend/config/SecurityConfig.java
+++ src/main/java/posts_management/backend/config/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("https://fe-test-woad.vercel.app/"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
