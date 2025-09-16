2613e62dc1a555895d5b522bd9c59f650da98871
Minh Trung <trungho.234416@gmail.com>
2025-09-16 09:55:30 +0700

    fix

--- src/main/java/posts_management/backend/config/SecurityConfig.java
+++ src/main/java/posts_management/backend/config/SecurityConfig.java
@@ -52,8 +52,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("/http://localhost:3000","https://fe-posts.vercel.app/login"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://fe-posts.vercel.app"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
